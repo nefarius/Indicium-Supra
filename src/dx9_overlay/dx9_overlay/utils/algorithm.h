796d2702b99255024d8b9a5a3b45589b21339367
agrippa1994 <agrippa1994@icloud.com>
2014-05-16 18:27:57 +0200

    Format changed

--- src/dx9_overlay/dx9_overlay/utils/algorithm.h
+++ src/dx9_overlay/dx9_overlay/utils/algorithm.h
@@ -4,7 +4,7 @@ template<typename MapContainer, typename Pred>
 void erase_if(MapContainer &c, Pred pred)
 {
 	for (auto p = c.begin(); p != c.end();)
-	if (pred(p->first, p->second))
-		p = c.erase(p);
-	else  ++p;
+		if (pred(p->first, p->second))
+			p = c.erase(p);
+		else  ++p;
 }
