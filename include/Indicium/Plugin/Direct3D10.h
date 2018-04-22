dc983281dcbd94c8a7613dd58df76271263070e7
Benjamin Höglinger <nefarius@dhmx.at>
2018-04-22 19:16:59 +0200

    Implemented common helper functions

--- include/Indicium/Plugin/Direct3D10.h
+++ include/Indicium/Plugin/Direct3D10.h
@@ -3,16 +3,22 @@
 
 #include "Common.h"
 #include <dxgi.h>
+#include <d3d10.h>
 
 INDICIUM_EXPORT VOID indicium_plugin_d3d10_present(
-    IDXGISwapChain  *pSwapChain, 
-    UINT            SyncInterval, 
+    IDXGISwapChain  *pSwapChain,
+    UINT            SyncInterval,
     UINT            Flags
 );
 
 INDICIUM_EXPORT VOID indicium_plugin_d3d10_resizetarget(
-    IDXGISwapChain          *pSwapChain, 
+    IDXGISwapChain          *pSwapChain,
     const DXGI_MODE_DESC    *pNewTargetParameters
 );
+
+HRESULT FORCEINLINE D3D10_DEVICE_FROM_SWAPCHAIN(IDXGISwapChain *pSwapChain, ID3D10Device **ppDevice)
+{
+    return pSwapChain->GetDevice(__uuidof(ID3D10Device), (PVOID*)ppDevice);
+}
 
 #endif // Direct3D10_h__
--- include/Indicium/Plugin/Direct3D11.h
+++ include/Indicium/Plugin/Direct3D11.h
@@ -3,6 +3,7 @@
 
 #include "Common.h"
 #include <dxgi.h>
+#include <d3d11.h>
 
 INDICIUM_EXPORT VOID indicium_plugin_d3d11_present(
     IDXGISwapChain  *pSwapChain,
@@ -14,5 +15,19 @@ INDICIUM_EXPORT VOID indicium_plugin_d3d11_resizetarget(
     IDXGISwapChain          *pSwapChain,
     const DXGI_MODE_DESC    *pNewTargetParameters
 );
+
+HRESULT FORCEINLINE D3D11_DEVICE_CONTEXT_FROM_SWAPCHAIN(
+    IDXGISwapChain          *pSwapChain,
+    ID3D11Device            **ppDevice,
+    ID3D11DeviceContext     **ppContext
+)
+{
+    HRESULT ret = pSwapChain->GetDevice(__uuidof(ID3D11Device), (PVOID*)ppDevice);
+
+    if (SUCCEEDED(ret))
+        (*ppDevice)->GetImmediateContext(ppContext);
+
+    return ret;
+}
 
 #endif // Direct3D11_h__
