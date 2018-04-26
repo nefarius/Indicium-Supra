e81b5f33f38dffd91ca3013d342271d286f96f34
Benjamin Höglinger <nefarius@dhmx.at>
2018-04-26 22:15:48 +0200

    Implemented render functions

--- src/Indicium-Supra/Utils/PluginManager.cpp
+++ src/Indicium-Supra/Utils/PluginManager.cpp
@@ -136,46 +136,90 @@ void PluginManager::load(Direct3DVersion version)
 void PluginManager::d3d9_present(LPDIRECT3DDEVICE9 pDevice, const RECT* pSourceRect, const RECT* pDestRect,
     HWND hDestWindowOverride, const RGNDATA* pDirtyRegion)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.d3d9.present(pDevice, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion);
+    }
 }
 
 void PluginManager::d3d9_reset(LPDIRECT3DDEVICE9 pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.d3d9.reset(pDevice, pPresentationParameters);
+    }
 }
 
 void PluginManager::d3d9_endscene(LPDIRECT3DDEVICE9 pDevice)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.d3d9.endscene(pDevice);
+    }
 }
 
 void PluginManager::d3d9_presentex(LPDIRECT3DDEVICE9EX pDevice, const RECT* pSourceRect, const RECT* pDestRect,
     HWND hDestWindowOverride, const RGNDATA* pDirtyRegion, DWORD dwFlags)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.d3d9ex.presentex(pDevice, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion, dwFlags);
+    }
 }
 
 void PluginManager::d3d9_resetex(LPDIRECT3DDEVICE9EX pDevice, D3DPRESENT_PARAMETERS* pPresentationParameters,
     D3DDISPLAYMODEEX* pFullscreenDisplayMode)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.d3d9ex.resetex(pDevice, pPresentationParameters, pFullscreenDisplayMode);
+    }
 }
 
 void PluginManager::d3d10_present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.present(pSwapChain, SyncInterval, Flags);
+    }
 }
 
 void PluginManager::d3d10_resizetarget(IDXGISwapChain* pSwapChain, const DXGI_MODE_DESC* pNewTargetParameters)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.resizetarget(pSwapChain, pNewTargetParameters);
+    }
 }
 
 void PluginManager::d3d11_present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.present(pSwapChain, SyncInterval, Flags);
+    }
 }
 
 void PluginManager::d3d11_resizetarget(IDXGISwapChain* pSwapChain, const DXGI_MODE_DESC* pNewTargetParameters)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.resizetarget(pSwapChain, pNewTargetParameters);
+    }
 }
 
 void PluginManager::d3d12_present(IDXGISwapChain* pSwapChain, UINT SyncInterval, UINT Flags)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.present(pSwapChain, SyncInterval, Flags);
+    }
 }
 
 void PluginManager::d3d12_resizetarget(IDXGISwapChain* pSwapChain, const DXGI_MODE_DESC* pNewTargetParameters)
 {
+    for (auto& plugin : _plugins)
+    {
+        plugin->function_ptrs.dxgi.resizetarget(pSwapChain, pNewTargetParameters);
+    }
 }
