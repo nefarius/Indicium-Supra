6cb69d638382d90f8c22e6ead11ebf69f7cf15ce
Benjamin Höglinger <nefarius@dhmx.at>
2018-04-26 17:34:22 +0200

    Removed unused code

--- src/Indicium-Supra/Game/Game.h
+++ src/Indicium-Supra/Game/Game.h
@@ -6,10 +6,5 @@
 
 void initGame();
 
-void HookDX9(UINTX* vtable9);
-void HookDX9Ex(UINTX* vtable9Ex);
-void HookDX10(UINTX* vtable10SwapChain);
-void HookDX11(UINTX* vtable11SwapChain);
-void HookDX12(UINTX* vtable12SwapChain);
 void HookDInput8(UINTX* vtable8);
 
