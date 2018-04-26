5f62c6d18e7173408c52513f52b54e1225cfe7b7
Benjamin Höglinger <nefarius@dhmx.at>
2018-04-26 21:20:59 +0200

    Added Plugin structure

--- src/Indicium-Supra/Utils/PluginManager.h
+++ src/Indicium-Supra/Utils/PluginManager.h
@@ -4,15 +4,18 @@
 #include <map>
 #include <Indicium/Plugin/Common.h>
 
+#include <d3d9.h>
+#include <dxgi.h>
+
 #define POCO_NO_UNWINDOWS
 #include <Poco/SharedLibrary.h>
 #include <Poco/Mutex.h>
-#include <d3d9.h>
-#include <dxgi.h>
+#include <Poco/SharedPtr.h>
 
 using Poco::SharedLibrary;
 using Poco::FastMutex;
 using Poco::ScopedLock;
+using Poco::SharedPtr;
 
 EXTERN_C IMAGE_DOS_HEADER __ImageBase;
 
@@ -25,7 +28,45 @@ class PluginManager
     std::map<std::string, std::vector<LPVOID>> _fpMap;
     std::vector<std::string> exports = { "Present", "Reset", "EndScene", "ResizeTarget" };
 
-    std::vector<std::string> d3d9_exports = { "" };
+    //
+    // D3D9(Ex)
+    // 
+    typedef HRESULT(WINAPI* fp_d3d9_present)(LPDIRECT3DDEVICE9, CONST RECT *, CONST RECT *, HWND, CONST RGNDATA *);
+    typedef HRESULT(WINAPI* fp_d3d9_reset)(LPDIRECT3DDEVICE9, D3DPRESENT_PARAMETERS *);
+    typedef HRESULT(WINAPI* fp_d3d9_endscene)(LPDIRECT3DDEVICE9);
+    typedef HRESULT(WINAPI* fp_d3d9_presentex)(LPDIRECT3DDEVICE9EX, CONST RECT *, CONST RECT *, HWND, CONST RGNDATA *, DWORD);
+    typedef HRESULT(WINAPI* fp_d3d9_resetex)(LPDIRECT3DDEVICE9EX, D3DPRESENT_PARAMETERS *, D3DDISPLAYMODEEX *);
+
+    //
+    // DXGI
+    // 
+    typedef HRESULT(WINAPI* fP_dxgi_present)(IDXGISwapChain*, UINT, UINT);
+    typedef HRESULT(WINAPI *fP_dxgi_resizetarget)(IDXGISwapChain*, const DXGI_MODE_DESC*);
+
+    struct Plugin
+    {
+        Direct3DVersion version;
+        SharedPtr<SharedLibrary> plugin_module;
+        union function_ptrs
+        {
+            struct d3d9
+            {
+                fp_d3d9_present present;
+                fp_d3d9_reset reset;
+                fp_d3d9_endscene endscene;
+            };
+            struct d3d9ex
+            {
+                fp_d3d9_presentex presentex;
+                fp_d3d9_resetex resetex;
+            };
+            struct dxgi
+            {
+                fP_dxgi_present present;
+                fP_dxgi_resizetarget resizetarget;
+            };
+        };
+    };
 
 public:
     PluginManager();
