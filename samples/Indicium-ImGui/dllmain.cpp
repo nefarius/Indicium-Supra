dca053b56a049ae92dcd4c894a40251648f9abda
Benjamin Höglinger <nefarius@dhmx.at>
2018-05-05 16:39:42 +0200

    Added license headers

--- samples/Indicium-ImGui/dllmain.cpp
+++ samples/Indicium-ImGui/dllmain.cpp
@@ -1,3 +1,27 @@
+/*
+MIT License
+
+Copyright (c) 2018 Benjamin Höglinger
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include "dllmain.h"
 
 // 
@@ -47,9 +71,26 @@ using Poco::Path;
 
 t_WindowProc OriginalWindowProc = nullptr;
 
-
+/**
+ * \fn  BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
+ *
+ * \brief   Your typical DLL entry point function. We're not doing much here since a special
+ *          initialization routine get's called upon getting loaded by Indicium-Supra.
+ *
+ * \author  Benjamin "Nefarius" Höglinger
+ * \date    05.05.2018
+ *
+ * \param   hInstance   The instance.
+ * \param   dwReason    The reason.
+ * \param   parameter3  The third parameter.
+ *
+ * \return  A WINAPI.
+ */
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
 {
+    //
+    // We don't need to get notified in thread attach- or detachments
+    // 
     DisableThreadLibraryCalls(static_cast<HMODULE>(hInstance));
 
     return TRUE;
