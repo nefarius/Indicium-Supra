0c8b12055b26f94f99da42f164cf8189c9434581
Benjamin Höglinger <nefarius@dhmx.at>
2018-06-12 19:42:02 +0200

    Implementing IndiciumEngineInit

--- src/Indicium-Supra/Engine.cpp
+++ src/Indicium-Supra/Engine.cpp
@@ -22,6 +22,10 @@ typedef struct _INDICIUM_ENGINE
 
     INDICIUM_D3D12_EVENT_CALLBACKS EventsD3D12;
 
+    HANDLE EngineThread;
+
+    HANDLE EngineCancellationEvent;
+
 } INDICIUM_ENGINE;
 
 
@@ -36,6 +40,41 @@ INDICIUM_API PINDICIUM_ENGINE IndiciumEngineAlloc(void)
     RtlZeroMemory(engine, sizeof(INDICIUM_ENGINE));
 
     return engine;
+}
+
+INDICIUM_API INDICIUM_ERROR IndiciumEngineInit(PINDICIUM_ENGINE Engine, PFN_EVT_INDICIUM_GAME_HOOKED EvtIndiciumGameHooked)
+{
+    if (!Engine) {
+        return INDICIUM_ERROR_INVALID_ENGINE_HANDLE;
+    }
+
+    if (Engine->EventsD3D9.Size != sizeof(INDICIUM_D3D9_EVENT_CALLBACKS)) {
+        return INDICIUM_ERROR_INVALID_D3D9_CALLBACKS_SIZE;
+    }
+
+    if (Engine->EventsD3D10.Size != sizeof(INDICIUM_D3D10_EVENT_CALLBACKS)) {
+        return INDICIUM_ERROR_INVALID_D3D10_CALLBACKS_SIZE;
+    }
+
+    if (Engine->EventsD3D11.Size != sizeof(INDICIUM_D3D11_EVENT_CALLBACKS)) {
+        return INDICIUM_ERROR_INVALID_D3D11_CALLBACKS_SIZE;
+    }
+
+    if (Engine->EventsD3D12.Size != sizeof(INDICIUM_D3D12_EVENT_CALLBACKS)) {
+        return INDICIUM_ERROR_INVALID_D3D12_CALLBACKS_SIZE;
+    }
+
+    //
+    // Callback invoked when initialization finished
+    // 
+    Engine->EvtIndiciumGameHooked = EvtIndiciumGameHooked;
+
+    //
+    // Event to notify engine thread about termination
+    // 
+    Engine->EngineCancellationEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+
+    return INDICIUM_ERROR_NONE;
 }
 
 INDICIUM_API VOID IndiciumEngineSetD3D9EventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_D3D9_EVENT_CALLBACKS Callbacks)
