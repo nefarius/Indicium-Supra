c3969756dcb4a1104b1add3cf53e5cadc9718743
Benjamin Höglinger <nefarius@dhmx.at>
2018-06-12 18:41:08 +0200

    Expanded struct _INDICIUM_ENGINE

--- src/Indicium-Supra/Engine.cpp
+++ src/Indicium-Supra/Engine.cpp
@@ -3,11 +3,51 @@
 
 #include "Indicium/Engine/IndiciumCore.h"
 #include "Indicium/Engine/IndiciumDirect3D9.h"
+#include "Indicium/Engine/IndiciumDirect3D10.h"
+#include "Indicium/Engine/IndiciumDirect3D11.h"
+#include "Indicium/Engine/IndiciumDirect3D12.h"
 
 
 typedef struct _INDICIUM_ENGINE
 {
     struct {
-        
+        PFN_INDICIUM_D3D9_PRESENT PrePresent;
+        PFN_INDICIUM_D3D9_PRESENT PostPresent;
+
+        PFN_INDICIUM_D3D9_RESET PreReset;
+        PFN_INDICIUM_D3D9_RESET PostReset;
+
+        PFN_INDICIUM_D3D9_END_SCENE PreEndScene;
+        PFN_INDICIUM_D3D9_END_SCENE PostEndScene;
+
+        PFN_INDICIUM_D3D9_PRESENT_EX PrePresentEx;
+        PFN_INDICIUM_D3D9_PRESENT_EX PostPresentEx;
+
+        PFN_INDICIUM_D3D9_RESET_EX PreResetEx;
+        PFN_INDICIUM_D3D9_RESET_EX PostResetEx;
     } EventsD3D9;
+
+    struct {
+        PFN_INDICIUM_D3D10_PRESENT PrePresent;
+        PFN_INDICIUM_D3D10_PRESENT PostPresent;
+
+        PFN_INDICIUM_D3D10_RESIZE_TARGET PreResizeTarget;
+        PFN_INDICIUM_D3D10_RESIZE_TARGET PostResizeTarget;
+    } EventsD3D10;
+
+    struct {
+        PFN_INDICIUM_D3D11_PRESENT PrePresent;
+        PFN_INDICIUM_D3D11_PRESENT PostPresent;
+
+        PFN_INDICIUM_D3D11_RESIZE_TARGET PreResizeTarget;
+        PFN_INDICIUM_D3D11_RESIZE_TARGET PostResizeTarget;
+    } EventsD3D11;
+
+    struct {
+        PFN_INDICIUM_D3D12_PRESENT PrePresent;
+        PFN_INDICIUM_D3D12_PRESENT PostPresent;
+
+        PFN_INDICIUM_D3D12_RESIZE_TARGET PreResizeTarget;
+        PFN_INDICIUM_D3D12_RESIZE_TARGET PostResizeTarget;
+    } EventsD3D12;
 } INDICIUM_ENGINE;
