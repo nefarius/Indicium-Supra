63a51d7d598ea87dac9017efb84b22525e71d61f
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-05-12 15:56:39 +0200

    Removed legacy include path from project

--- samples/Indicium-ImGui/dllmain.cpp
+++ samples/Indicium-ImGui/dllmain.cpp
@@ -34,6 +34,20 @@ SOFTWARE.
 // 
 #include <mutex>
 
+//
+// Boost
+//
+#include <boost/log/core.hpp>
+#include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/attributes/named_scope.hpp>
+
+namespace logging = boost::log;
+namespace keywords = boost::log::keywords;
+namespace attrs = boost::log::attributes;
+
 // 
 // POCO
 // 
