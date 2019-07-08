01d4f0871389b034e445b16e3e60f204b0520811
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-07-08 19:30:23 +0200

    Changed default CI/CLI build to static linkage

--- build/Build.cs
+++ build/Build.cs
@@ -15,7 +15,7 @@ class Build : NukeBuild
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly string Configuration = IsLocalBuild ? "Debug" : "Release";
+    readonly string Configuration = IsLocalBuild ? "Debug_LIB" : "Release_LIB";
 
     [Solution("Indicium-Supra.sln")] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
