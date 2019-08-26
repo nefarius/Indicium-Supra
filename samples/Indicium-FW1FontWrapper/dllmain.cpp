b4443bde524bebbbef8645cf641a3fd518e2d292
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-08-26 23:15:11 +0200

    Fixed Indicium-FW1FontWrapper to use new API

--- samples/Indicium-FW1FontWrapper/dllmain.cpp
+++ samples/Indicium-FW1FontWrapper/dllmain.cpp
@@ -27,8 +27,8 @@ SOFTWARE.
 
 #include "FW1FontWrapper.h"
 
-#include <Indicium\Engine\IndiciumCore.h>
 #include <Indicium\Engine\IndiciumDirect3D11.h>
+#include <cassert>
 
 #ifdef _M_IX86
 #pragma comment(lib, "x86\\FW1FontWrapper.lib")
@@ -38,98 +38,169 @@ SOFTWARE.
 #pragma comment(lib, "dxguid.lib")
 
 
-PINDICIUM_ENGINE engine = nullptr;
-ID3D11DeviceContext* ctx = nullptr;
-ID3D11Device* dev = nullptr;
-IFW1Factory *pFW1Factory = nullptr;
-IFW1FontWrapper *pFontWrapper = nullptr;
+typedef struct _FW1_CTX
+{
+	ID3D11DeviceContext* ctx;
+	ID3D11Device* dev;
+	IFW1Factory *pFW1Factory;
+	IFW1FontWrapper *pFontWrapper;
 
-EVT_INDICIUM_D3D11_PRESENT EvtIndiciumD3D11Present;
+} FW1_CTX, *PFW1_CTX;
 
+EVT_INDICIUM_GAME_HOOKED EvtIndiciumGameHooked;
+EVT_INDICIUM_D3D11_PRE_PRESENT EvtIndiciumD3D11PrePresent;
+EVT_INDICIUM_GAME_UNHOOKED EvtIndiciumGamePostUnhooked;
 
 /**
- * \fn  BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
+ * \fn	BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
  *
- * \brief   Your typical DLL entry point function. We're not doing much here since a special
- *          initialization routine gets called upon getting loaded by Indicium-Supra.
+ * \brief	Your typical DLL entry point function. We're not doing much here since a special
+ * 			initialization routine gets called upon getting loaded by Indicium-Supra.
  *
- * \author  Benjamin "Nefarius" Höglinger
- * \date    05.05.2018
+ * \author	Benjamin "Nefarius" Höglinger
+ * \date	05.05.2018
  *
- * \param   hInstance   The instance.
- * \param   dwReason    The reason.
- * \param   parameter3  The third parameter.
+ * \param	hInstance 	The instance.
+ * \param	dwReason  	The reason.
+ * \param	parameter3	The third parameter.
  *
- * \return  A WINAPI.
+ * \returns	A WINAPI.
  */
 BOOL WINAPI DllMain(HINSTANCE hInstance, DWORD dwReason, LPVOID)
 {
-    //
-    // We don't need to get notified in thread attach- or detachments
-    // 
-    DisableThreadLibraryCalls(static_cast<HMODULE>(hInstance));
+	//
+	// We don't need to get notified in thread attach- or detachments
+	// 
+	DisableThreadLibraryCalls(static_cast<HMODULE>(hInstance));
 
-    INDICIUM_D3D11_EVENT_CALLBACKS d3d11;
-    INDICIUM_D3D11_EVENT_CALLBACKS_INIT(&d3d11);
-    d3d11.EvtIndiciumD3D11PrePresent = EvtIndiciumD3D11Present;
+	INDICIUM_ENGINE_CONFIG cfg;
+	INDICIUM_ENGINE_CONFIG_INIT(&cfg);
 
-    switch (dwReason)
-    {
-    case DLL_PROCESS_ATTACH:
+	// Only attempt to detect and hook D3D11
+	cfg.Direct3D.HookDirect3D11 = TRUE;
+	// Called once game as been hooked
+	cfg.EvtIndiciumGameHooked = EvtIndiciumGameHooked;
+	// Called after hooks have been removed
+	cfg.EvtIndiciumGamePostUnhook = EvtIndiciumGamePostUnhooked;
 
-        if (!engine)
-        {
-            engine = IndiciumEngineAlloc();
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
 
-            IndiciumEngineSetD3D11EventCallbacks(engine, &d3d11);
+		//
+		// Bootstrap the engine. Allocates resources, establishes hooks etc.
+		// 
+		(void)IndiciumEngineCreate(
+			static_cast<HMODULE>(hInstance),
+			&cfg,
+			NULL
+		);
 
-            (void) IndiciumEngineInit(engine, nullptr);
-        }
+		break;
+	case DLL_PROCESS_DETACH:
 
-        break;
-    case DLL_PROCESS_DETACH:
+		//
+		// Tears down the engine. Graceful shutdown, frees resources etc.
+		// 
+		(void)IndiciumEngineDestroy(static_cast<HMODULE>(hInstance));
 
-        if (engine)
-        {
-            IndiciumEngineShutdown(engine, nullptr);
-            IndiciumEngineFree(engine);
-            engine = nullptr;
+		break;
+	default:
+		break;
+	}
 
-            pFontWrapper->Release();
-            pFontWrapper = nullptr;
-            pFW1Factory->Release();
-            pFW1Factory = nullptr;
-        }
-
-        break;
-    default:
-        break;
-    }
-
-    return TRUE;
+	return TRUE;
 }
 
-void EvtIndiciumD3D11Present(
-    IDXGISwapChain  *pSwapChain,
-    UINT            SyncInterval,
-    UINT            Flags
+//
+// Essential game functions successfully hooked, do further bootstrapping here
+// 
+void EvtIndiciumGameHooked(
+	PINDICIUM_ENGINE EngineHandle,
+	const INDICIUM_D3D_VERSION GameVersion
 )
 {
-    D3D11_DEVICE_IMMEDIATE_CONTEXT_FROM_SWAPCHAIN(pSwapChain, &dev, &ctx);
+	//
+	// At this stage we assume D3D11
+	// 
+	assert(GameVersion == IndiciumDirect3DVersion11);
 
-    if (!pFW1Factory)
-        (void) FW1CreateFactory(FW1_VERSION, &pFW1Factory);
+	PFW1_CTX pCtx = nullptr;
 
-    if (!pFontWrapper)
-        (void) pFW1Factory->CreateFontWrapper(dev, L"Arial", &pFontWrapper);
+	//
+	// Allocate context memory
+	// 
+	assert(IndiciumEngineAllocCustomContext(
+		EngineHandle,
+		(PVOID*)&pCtx,
+		sizeof(FW1_CTX)
+	) == INDICIUM_ERROR_NONE);
 
-    pFontWrapper->DrawString(
-        ctx,
-        L"Injected via Indicium-Supra by Nefarius",// String
-        50.0f,// Font size
-        15.0f,// X position
-        30.0f,// Y position
-        0xff0099ff,// Text color, 0xAaBbGgRr
-        FW1_RESTORESTATE// Flags (for example FW1_RESTORESTATE to keep context states unchanged)
-    );
+	INDICIUM_D3D11_EVENT_CALLBACKS d3d11;
+	INDICIUM_D3D11_EVENT_CALLBACKS_INIT(&d3d11);
+	d3d11.EvtIndiciumD3D11PrePresent = EvtIndiciumD3D11PrePresent;
+
+	// Begin invoking render hook callbacks
+	IndiciumEngineSetD3D11EventCallbacks(EngineHandle, &d3d11);
+}
+
+//
+// Game unloading, hooks are removed
+// 
+void EvtIndiciumGamePostUnhooked(PINDICIUM_ENGINE EngineHandle)
+{
+	const PFW1_CTX pCtx = PFW1_CTX(IndiciumEngineGetCustomContext(EngineHandle));
+
+	if (pCtx->pFontWrapper)
+		pCtx->pFontWrapper->Release();
+	if (pCtx->pFW1Factory)
+		pCtx->pFW1Factory->Release();
+}
+
+//
+// Present is about to get called
+// 
+void EvtIndiciumD3D11PrePresent(
+	IDXGISwapChain					*pSwapChain,
+	UINT							SyncInterval,
+	UINT							Flags,
+	PINDICIUM_EVT_PRE_EXTENSION     Extension
+)
+{
+	// Grab shared context
+	const PFW1_CTX pCtx = PFW1_CTX(Extension->Context);
+	ID3D11Device *pDeviceTmp;
+
+	if (!pCtx->pFW1Factory)
+		(void)FW1CreateFactory(FW1_VERSION, &pCtx->pFW1Factory);
+
+	/*
+	 * Swapchain associated Device and Context pointers can become invalid
+	 * when the host process destroys and re-creates them (like RetroArch
+	 * does when switching cores) so compare to ones grabbed earlier and
+	 * re-request both if necessary.
+	 */
+	if (FAILED(D3D11_DEVICE_FROM_SWAPCHAIN(pSwapChain, &pDeviceTmp))) {
+		IndiciumEngineLogError("Failed to get device pointer from swapchain");
+		return;
+	}
+	else if (pCtx->dev != pDeviceTmp) {
+		D3D11_DEVICE_IMMEDIATE_CONTEXT_FROM_SWAPCHAIN(
+			pSwapChain,
+			&pCtx->dev,
+			&pCtx->ctx
+		);
+
+		(void)pCtx->pFW1Factory->CreateFontWrapper(pCtx->dev, L"Arial", &pCtx->pFontWrapper);
+	}
+
+	pCtx->pFontWrapper->DrawString(
+		pCtx->ctx,
+		L"Injected via Indicium-Supra by Nefarius",// String
+		50.0f,// Font size
+		15.0f,// X position
+		30.0f,// Y position
+		0xff0099ff,// Text color, 0xAaBbGgRr
+		FW1_RESTORESTATE// Flags (for example FW1_RESTORESTATE to keep context states unchanged)
+	);
 }
