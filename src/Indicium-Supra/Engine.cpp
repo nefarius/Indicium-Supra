a4e6d5209854a9cbf1b2fc15745cacacae3549aa
Benjamin Höglinger-Stelzer <benjamin.hoeglinger@nefarius.at>
2019-09-09 15:49:51 +0200

    Refined fix for failing CreateEvent call Added error code INDICIUM_ERROR_CREATE_EVENT_FAILED

--- src/Indicium-Supra/Engine.cpp
+++ src/Indicium-Supra/Engine.cpp
@@ -63,194 +63,206 @@ static std::map<HMODULE, PINDICIUM_ENGINE> g_EngineHostInstances;
 
 INDICIUM_API INDICIUM_ERROR IndiciumEngineCreate(HMODULE HostInstance, PINDICIUM_ENGINE_CONFIG EngineConfig, PINDICIUM_ENGINE * Engine)
 {
-    //
-    // Check if we got initialized for this instance before
-    // 
-    if (g_EngineHostInstances.count(HostInstance)) {
-        return INDICIUM_ERROR_ENGINE_ALREADY_ALLOCATED;
-    }
+	//
+	// Check if we got initialized for this instance before
+	// 
+	if (g_EngineHostInstances.count(HostInstance)) {
+		return INDICIUM_ERROR_ENGINE_ALREADY_ALLOCATED;
+	}
 
-    //
-    // Increase host DLL reference count
-    // 
-    HMODULE hmod;
-    if (!GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
-        reinterpret_cast<LPCTSTR>(HostInstance),
-        &hmod)) {
-        return INDICIUM_ERROR_REFERENCE_INCREMENT_FAILED;
-    }
+	//
+	// Increase host DLL reference count
+	// 
+	HMODULE hmod;
+	if (!GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
+		reinterpret_cast<LPCTSTR>(HostInstance),
+		&hmod)) {
+		return INDICIUM_ERROR_REFERENCE_INCREMENT_FAILED;
+	}
 
-    const auto engine = static_cast<PINDICIUM_ENGINE>(malloc(sizeof(INDICIUM_ENGINE)));
+	const auto engine = static_cast<PINDICIUM_ENGINE>(malloc(sizeof(INDICIUM_ENGINE)));
 
-    if (!engine) {
-        return INDICIUM_ERROR_ENGINE_ALLOCATION_FAILED;
-    }
+	if (!engine) {
+		return INDICIUM_ERROR_ENGINE_ALLOCATION_FAILED;
+	}
 
-    ZeroMemory(engine, sizeof(INDICIUM_ENGINE));
-    engine->HostInstance = HostInstance;
-    CopyMemory(&engine->EngineConfig, EngineConfig, sizeof(INDICIUM_ENGINE_CONFIG));
+	//
+	// Initialize engine handle
+	// 
+	ZeroMemory(engine, sizeof(INDICIUM_ENGINE));
+	engine->HostInstance = HostInstance;
+	CopyMemory(&engine->EngineConfig, EngineConfig, sizeof(INDICIUM_ENGINE_CONFIG));
 
-    //
-    // Event to notify engine thread about termination
-    // 
-    engine->EngineCancellationEvent = CreateEvent(nullptr, FALSE, FALSE, TEXT("IndiciumRandomString"));
-    auto dwLastError = GetLastError();
-    //
-    // Set up logging
-    //
-    auto logger = spdlog::basic_logger_mt(
-        "indicium",
-        Indicium::Core::Util::expand_environment_variables(EngineConfig->Logging.FilePath)
-    );
+	//
+	// Set up logging
+	//
+	auto logger = spdlog::basic_logger_mt(
+		"indicium",
+		Indicium::Core::Util::expand_environment_variables(EngineConfig->Logging.FilePath)
+	);
 
 #if _DEBUG
-    spdlog::set_level(spdlog::level::debug);
-    logger->flush_on(spdlog::level::debug);
+	spdlog::set_level(spdlog::level::debug);
+	logger->flush_on(spdlog::level::debug);
 #else
-    logger->flush_on(spdlog::level::info);
+	logger->flush_on(spdlog::level::info);
 #endif
 
-    if (EngineConfig->Logging.IsEnabled) {
-        spdlog::set_default_logger(logger);
-    }
+	if (EngineConfig->Logging.IsEnabled) {
+		spdlog::set_default_logger(logger);
+	}
 
-    logger = spdlog::get("indicium")->clone("api");
+	logger = spdlog::get("indicium")->clone("api");
 
-    logger->info("Indicium engine initialized, attempting to launch main thread" );
-    if (engine->EngineCancellationEvent == INVALID_HANDLE_VALUE) {
-        logger->error("Failed to create the Engine Cancellation Event {}." dwLastError);
-    }
-    //
-    // Kickstart hooking the rendering pipeline
-    // 
-    engine->EngineThread = CreateThread(
-        nullptr,
-        0,
-        reinterpret_cast<LPTHREAD_START_ROUTINE>(IndiciumMainThread),
-        engine,
-        0,
-        nullptr
-    );
+	//
+	// Event to notify engine thread about termination
+	// 
+	engine->EngineCancellationEvent = CreateEvent(
+		nullptr,
+		FALSE, // Auto-reset event
+		FALSE, // Initial state non-signaled
+		TEXT("IndiciumEngineCancellationEvent") // Named unique event
+	);
 
-    if (!engine->EngineThread) {
-        logger->error("Could not create main thread, library unusable");
-        return INDICIUM_ERROR_CREATE_THREAD_FAILED;
-    }
+	if (engine->EngineCancellationEvent == INVALID_HANDLE_VALUE
+		|| engine->EngineCancellationEvent == NULL) {
+		logger->error("Failed to create the Engine Cancellation Event: {}", GetLastError());
+		return INDICIUM_ERROR_CREATE_EVENT_FAILED;
+	}
 
-    logger->info("Main thread created successfully");
+	logger->info("Indicium engine initialized, attempting to launch main thread");
 
-    if (Engine)
-    {
-        *Engine = engine;
-    }
+	//
+	// Kickstart hooking the rendering pipeline
+	// 
+	engine->EngineThread = CreateThread(
+		nullptr,
+		0,
+		reinterpret_cast<LPTHREAD_START_ROUTINE>(IndiciumMainThread),
+		engine,
+		0,
+		nullptr
+	);
 
-    g_EngineHostInstances[HostInstance] = engine;
+	if (!engine->EngineThread) {
+		logger->error("Could not create main thread, library unusable");
+		return INDICIUM_ERROR_CREATE_THREAD_FAILED;
+	}
 
-    return INDICIUM_ERROR_NONE;
+	logger->info("Main thread created successfully");
+
+	if (Engine)
+	{
+		*Engine = engine;
+	}
+
+	g_EngineHostInstances[HostInstance] = engine;
+
+	return INDICIUM_ERROR_NONE;
 }
 
 INDICIUM_API INDICIUM_ERROR IndiciumEngineDestroy(HMODULE HostInstance)
 {
-    if (!g_EngineHostInstances.count(HostInstance)) {
-        return INDICIUM_ERROR_INVALID_HMODULE_HANDLE;
-    }
+	if (!g_EngineHostInstances.count(HostInstance)) {
+		return INDICIUM_ERROR_INVALID_HMODULE_HANDLE;
+	}
 
-    const auto& engine = g_EngineHostInstances[HostInstance];
-    auto logger = spdlog::get("indicium")->clone("api");
+	const auto& engine = g_EngineHostInstances[HostInstance];
+	auto logger = spdlog::get("indicium")->clone("api");
 
-    logger->info("Freeing remaining resources");
+	logger->info("Freeing remaining resources");
 
-    CloseHandle(engine->EngineCancellationEvent);
-    CloseHandle(engine->EngineThread);
+	CloseHandle(engine->EngineCancellationEvent);
+	CloseHandle(engine->EngineThread);
 
-    const auto it = g_EngineHostInstances.find(HostInstance);
-    g_EngineHostInstances.erase(it);
+	const auto it = g_EngineHostInstances.find(HostInstance);
+	g_EngineHostInstances.erase(it);
 
-    logger->info("Engine shutdown complete");
-    logger->flush();
+	logger->info("Engine shutdown complete");
+	logger->flush();
 
-    return INDICIUM_ERROR_NONE;
+	return INDICIUM_ERROR_NONE;
 }
 
 INDICIUM_API INDICIUM_ERROR IndiciumEngineAllocCustomContext(PINDICIUM_ENGINE Engine, PVOID* Context, size_t ContextSize)
 {
-    if (!Engine) {
-        return INDICIUM_ERROR_INVALID_ENGINE_HANDLE;
-    }
+	if (!Engine) {
+		return INDICIUM_ERROR_INVALID_ENGINE_HANDLE;
+	}
 
-    if (Engine->CustomContext) {
-        IndiciumEngineFreeCustomContext(Engine);
-    }
+	if (Engine->CustomContext) {
+		IndiciumEngineFreeCustomContext(Engine);
+	}
 
-    Engine->CustomContext = malloc(ContextSize);
+	Engine->CustomContext = malloc(ContextSize);
 
-    if (!Engine->CustomContext) {
-        return INDICIUM_ERROR_CONTEXT_ALLOCATION_FAILED;
-    }
+	if (!Engine->CustomContext) {
+		return INDICIUM_ERROR_CONTEXT_ALLOCATION_FAILED;
+	}
 
-    ZeroMemory(Engine->CustomContext, ContextSize);
-    *Context = Engine->CustomContext;
+	ZeroMemory(Engine->CustomContext, ContextSize);
+	*Context = Engine->CustomContext;
 
-    return INDICIUM_ERROR_NONE;
+	return INDICIUM_ERROR_NONE;
 }
 
 INDICIUM_API INDICIUM_ERROR IndiciumEngineFreeCustomContext(PINDICIUM_ENGINE Engine)
 {
-    if (!Engine) {
-        return INDICIUM_ERROR_INVALID_ENGINE_HANDLE;
-    }
+	if (!Engine) {
+		return INDICIUM_ERROR_INVALID_ENGINE_HANDLE;
+	}
 
-    if (Engine->CustomContext) {
-        free(Engine->CustomContext);
-    }
+	if (Engine->CustomContext) {
+		free(Engine->CustomContext);
+	}
 
-    return INDICIUM_ERROR_NONE;
+	return INDICIUM_ERROR_NONE;
 }
 
 INDICIUM_API PVOID IndiciumEngineGetCustomContext(PINDICIUM_ENGINE Engine)
 {
-    if (!Engine) {
-        return nullptr;
-    }
+	if (!Engine) {
+		return nullptr;
+	}
 
-    return Engine->CustomContext;
+	return Engine->CustomContext;
 }
 
 #ifndef INDICIUM_NO_D3D9
 
 INDICIUM_API VOID IndiciumEngineSetD3D9EventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_D3D9_EVENT_CALLBACKS Callbacks)
 {
-    if (Engine) {
-        Engine->EventsD3D9 = *Callbacks;
-    }
+	if (Engine) {
+		Engine->EventsD3D9 = *Callbacks;
+	}
 }
 
 INDICIUM_API PINDICIUM_ENGINE IndiciumEngineGetHandleFromD3D9Device(LPDIRECT3DDEVICE9 Device)
 {
-    for (const auto& kv : g_EngineHostInstances)
-    {
-        const auto& engine = kv.second;
+	for (const auto& kv : g_EngineHostInstances)
+	{
+		const auto& engine = kv.second;
 
-        if (engine->RenderPipeline.pD3D9Device == Device) {
-            return engine;
-        }
-    }
+		if (engine->RenderPipeline.pD3D9Device == Device) {
+			return engine;
+		}
+	}
 
-    return nullptr;
+	return nullptr;
 }
 
 INDICIUM_API PINDICIUM_ENGINE IndiciumEngineGetHandleFromD3D9ExDevice(LPDIRECT3DDEVICE9EX Device)
 {
-    for (const auto& kv : g_EngineHostInstances)
-    {
-        const auto& engine = kv.second;
+	for (const auto& kv : g_EngineHostInstances)
+	{
+		const auto& engine = kv.second;
 
-        if (engine->RenderPipeline.pD3D9ExDevice == Device) {
-            return engine;
-        }
-    }
+		if (engine->RenderPipeline.pD3D9ExDevice == Device) {
+			return engine;
+		}
+	}
 
-    return nullptr;
+	return nullptr;
 }
 
 #endif
@@ -259,9 +271,9 @@ INDICIUM_API PINDICIUM_ENGINE IndiciumEngineGetHandleFromD3D9ExDevice(LPDIRECT3D
 
 INDICIUM_API VOID IndiciumEngineSetD3D10EventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_D3D10_EVENT_CALLBACKS Callbacks)
 {
-    if (Engine) {
-        Engine->EventsD3D10 = *Callbacks;
-    }
+	if (Engine) {
+		Engine->EventsD3D10 = *Callbacks;
+	}
 }
 
 #endif
@@ -270,9 +282,9 @@ INDICIUM_API VOID IndiciumEngineSetD3D10EventCallbacks(PINDICIUM_ENGINE Engine, 
 
 INDICIUM_API VOID IndiciumEngineSetD3D11EventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_D3D11_EVENT_CALLBACKS Callbacks)
 {
-    if (Engine) {
-        Engine->EventsD3D11 = *Callbacks;
-    }
+	if (Engine) {
+		Engine->EventsD3D11 = *Callbacks;
+	}
 }
 
 #endif
@@ -281,9 +293,9 @@ INDICIUM_API VOID IndiciumEngineSetD3D11EventCallbacks(PINDICIUM_ENGINE Engine, 
 
 INDICIUM_API VOID IndiciumEngineSetD3D12EventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_D3D12_EVENT_CALLBACKS Callbacks)
 {
-    if (Engine) {
-        Engine->EventsD3D12 = *Callbacks;
-    }
+	if (Engine) {
+		Engine->EventsD3D12 = *Callbacks;
+	}
 }
 
 #endif
@@ -292,53 +304,53 @@ INDICIUM_API VOID IndiciumEngineSetD3D12EventCallbacks(PINDICIUM_ENGINE Engine, 
 
 INDICIUM_API VOID IndiciumEngineSetARCEventCallbacks(PINDICIUM_ENGINE Engine, PINDICIUM_ARC_EVENT_CALLBACKS Callbacks)
 {
-    if (Engine) {
-        Engine->EventsARC = *Callbacks;
-    }
+	if (Engine) {
+		Engine->EventsARC = *Callbacks;
+	}
 }
 
 #endif
 
 INDICIUM_API VOID IndiciumEngineLogDebug(LPCSTR Format, ...)
 {
-    auto logger = spdlog::get("indicium")->clone("host");
-    va_list args;
-    char buf[1000]; // TODO: dumb, make better
-    va_start(args, Format);
-    vsnprintf(buf, sizeof(buf), Format, args);
-    va_end(args);
-    logger->debug(buf);
+	auto logger = spdlog::get("indicium")->clone("host");
+	va_list args;
+	char buf[1000]; // TODO: dumb, make better
+	va_start(args, Format);
+	vsnprintf(buf, sizeof(buf), Format, args);
+	va_end(args);
+	logger->debug(buf);
 }
 
 INDICIUM_API VOID IndiciumEngineLogInfo(LPCSTR Format, ...)
 {
-    auto logger = spdlog::get("indicium")->clone("host");
-    va_list args;
-    char buf[1000]; // TODO: dumb, make better
-    va_start(args, Format);
-    vsnprintf(buf, sizeof(buf), Format, args);
-    va_end(args);
-    logger->info(buf);
+	auto logger = spdlog::get("indicium")->clone("host");
+	va_list args;
+	char buf[1000]; // TODO: dumb, make better
+	va_start(args, Format);
+	vsnprintf(buf, sizeof(buf), Format, args);
+	va_end(args);
+	logger->info(buf);
 }
 
 INDICIUM_API VOID IndiciumEngineLogWarning(LPCSTR Format, ...)
 {
-    auto logger = spdlog::get("indicium")->clone("host");
-    va_list args;
-    char buf[1000]; // TODO: dumb, make better
-    va_start(args, Format);
-    vsnprintf(buf, sizeof(buf), Format, args);
-    va_end(args);
-    logger->warn(buf);
+	auto logger = spdlog::get("indicium")->clone("host");
+	va_list args;
+	char buf[1000]; // TODO: dumb, make better
+	va_start(args, Format);
+	vsnprintf(buf, sizeof(buf), Format, args);
+	va_end(args);
+	logger->warn(buf);
 }
 
 INDICIUM_API VOID IndiciumEngineLogError(LPCSTR Format, ...)
 {
-    auto logger = spdlog::get("indicium")->clone("host");
-    va_list args;
-    char buf[1000]; // TODO: dumb, make better
-    va_start(args, Format);
-    vsnprintf(buf, sizeof(buf), Format, args);
-    va_end(args);
-    logger->error(buf);
+	auto logger = spdlog::get("indicium")->clone("host");
+	va_list args;
+	char buf[1000]; // TODO: dumb, make better
+	va_start(args, Format);
+	vsnprintf(buf, sizeof(buf), Format, args);
+	va_end(args);
+	logger->error(buf);
 }
