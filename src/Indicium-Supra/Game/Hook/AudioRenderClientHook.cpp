9c890c319da709c70bf2f5044cfb00b90645966a
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2020-01-29 19:13:42 +0100

    Reduced memory consumption

--- src/Indicium-Supra/Game/Hook/AudioRenderClientHook.cpp
+++ src/Indicium-Supra/Game/Hook/AudioRenderClientHook.cpp
@@ -30,99 +30,102 @@ using namespace Indicium::Core::Exceptions;
 
 CoreAudioHooking::AudioRenderClientHook::AudioRenderClientHook()
 {
-    // Note: may fail if already called by host
-    (void)CoInitializeEx(NULL, COINIT_MULTITHREADED);
+	// Note: may fail if already called by host
+	(void)CoInitializeEx(nullptr, COINIT_MULTITHREADED);
 
-    HRESULT hr = CoCreateInstance(
-        __uuidof(MMDeviceEnumerator),
-        NULL,
-        CLSCTX_ALL,
-        __uuidof(IMMDeviceEnumerator),
-        reinterpret_cast<void**>(&enumerator)
-    );
-    if (FAILED(hr)) {
-        throw ARCException("CoCreateInstance failed", hr);
-    }
+	HRESULT hr = CoCreateInstance(
+		__uuidof(MMDeviceEnumerator),
+		nullptr,
+		CLSCTX_ALL,
+		__uuidof(IMMDeviceEnumerator),
+		reinterpret_cast<void**>(&enumerator)
+	);
+	if (FAILED(hr))
+	{
+		throw ARCException("CoCreateInstance failed", hr);
+	}
 
-    hr = enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &device);
-    if (FAILED(hr)) {
-        throw ARCException("Failed to get default audio endpoint", hr);
-    }
+	hr = enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &device);
+	if (FAILED(hr))
+	{
+		throw ARCException("Failed to get default audio endpoint", hr);
+	}
 
-    hr = device->Activate(
-        __uuidof(IAudioClient),
-        CLSCTX_ALL,
-        NULL,
-        reinterpret_cast<void**>(&client)
-    );
-    if (FAILED(hr)) {
-        throw ARCException("Failed to activate IAudioClient instance", hr);
-    }
+	hr = device->Activate(
+		__uuidof(IAudioClient),
+		CLSCTX_ALL,
+		nullptr,
+		reinterpret_cast<void**>(&client)
+	);
+	if (FAILED(hr))
+	{
+		throw ARCException("Failed to activate IAudioClient instance", hr);
+	}
 
-    client->GetMixFormat(&pwfx);
+	client->GetMixFormat(&pwfx);
 
-    hr = client->Initialize(
-        AUDCLNT_SHAREMODE_SHARED,
-        0,
-        10 * 1000 * 1000,
-        0,
-        pwfx,
-        NULL
-    );
-    if ((S_OK != hr) && (pwfx->nSamplesPerSec != 48000))
-    {
-        pwfx->nSamplesPerSec = 48000;
-        hr = client->Initialize(
-            AUDCLNT_SHAREMODE_SHARED,
-            0,
-            10 * 1000 * 1000,
-            0,
-            pwfx,
-            NULL
-        );
-    }
-    else if ((S_OK != hr) && (pwfx->nSamplesPerSec != 44100))
-    {
-        pwfx->nSamplesPerSec = 44100;
-        hr = client->Initialize(
-            AUDCLNT_SHAREMODE_SHARED,
-            0,
-            10 * 1000 * 1000,
-            0,
-            pwfx,
-            NULL
-        );
-    }
-    if (FAILED(hr)) {
-        throw ARCException("Failed to initialize IAudioClient instance", hr);
-    }
+	hr = client->Initialize(
+		AUDCLNT_SHAREMODE_SHARED,
+		0,
+		10 * 1000 * 1000,
+		0,
+		pwfx,
+		nullptr
+	);
+	if ((S_OK != hr) && (pwfx->nSamplesPerSec != 48000))
+	{
+		pwfx->nSamplesPerSec = 48000;
+		hr = client->Initialize(
+			AUDCLNT_SHAREMODE_SHARED,
+			0,
+			10 * 1000 * 1000,
+			0,
+			pwfx,
+			nullptr
+		);
+	}
+	else if ((S_OK != hr) && (pwfx->nSamplesPerSec != 44100))
+	{
+		pwfx->nSamplesPerSec = 44100;
+		hr = client->Initialize(
+			AUDCLNT_SHAREMODE_SHARED,
+			0,
+			10 * 1000 * 1000,
+			0,
+			pwfx,
+			nullptr
+		);
+	}
+	if (FAILED(hr))
+	{
+		throw ARCException("Failed to initialize IAudioClient instance", hr);
+	}
 
-    hr = client->GetService(
-        __uuidof(IAudioRenderClient),
-        reinterpret_cast<void**>(&arc)
-    );
-    if (FAILED(hr)) {
-        throw ARCException("Failed to request IAudioRenderClient service", hr);
-    }
+	hr = client->GetService(
+		__uuidof(IAudioRenderClient),
+		reinterpret_cast<void**>(&arc)
+	);
+	if (FAILED(hr))
+	{
+		throw ARCException("Failed to request IAudioRenderClient service", hr);
+	}
 }
 
 CoreAudioHooking::AudioRenderClientHook::~AudioRenderClientHook()
 {
-    if (arc)
-        arc->Release();
-    if (client)
-        client->Release();
-    if (device)
-        device->Release();
-    if (enumerator)
-        enumerator->Release();
-    if (pwfx)
-        CoTaskMemFree(pwfx);
+	if (arc)
+		arc->Release();
+	if (client)
+		client->Release();
+	if (device)
+		device->Release();
+	if (enumerator)
+		enumerator->Release();
+	if (pwfx)
+		CoTaskMemFree(pwfx);
 }
 
 std::vector<size_t> CoreAudioHooking::AudioRenderClientHook::vtable() const
 {
-    size_t vtbl[VTableElements];
-    memcpy(vtbl, *reinterpret_cast<size_t **>(arc), VTableElements * sizeof(size_t));
-    return std::vector<size_t>(vtbl, vtbl + sizeof vtbl / sizeof vtbl[0]);
+	return std::vector<size_t>(*reinterpret_cast<size_t**>(arc), *reinterpret_cast<size_t**>(arc) + VTableElements);
 }
